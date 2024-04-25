95ca4eb7dc3f6288f41fc38fe857631650f14ce4
AkhtarAli7847 <Akhtaraliuog@gmail.com>
2024-04-26 03:32:46 +0500

    initial

---
+++ InMemoryCaching/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using InMemoryCaching.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IMemoryCache _cache;
+        public List<Employee> empData { get; set; }
+        public EmployeeController(IMemoryCache cache)
+        {
+            _cache = cache;
+            empData=new List<Employee>()
+            {
+                new Employee() {Id=1,empName="Robin"},
+                new Employee() {Id=2,empName="Clark"},
+                new Employee() {Id=3,empName="Michal"},
+                new Employee() {Id=4,empName="Stephin"},
+            };
+        }
+        [HttpGet("GetCachedEmployees")]
+        public IActionResult GetCachedEmployees()
+        {
+            Employee employee = new Employee();
+            var retCache = _cache.TryGetValue("employeeList", out List<Employee> employees);
+            if (!retCache)
+            {
+                employees = empData;
+                _cache.Set("employeeList", employees, TimeSpan.FromMinutes(1));
+            }
+            return Ok(employees);
+        }
+    }
+}
---
+++ InMemoryCaching/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace InMemoryCaching.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string empName { get; set; }
+    }
+}
